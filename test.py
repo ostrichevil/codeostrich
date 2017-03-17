4a3bc37489e725446a3bfa28fc8b82164ca9c34e
ostrichevil <13718246993@163.com>
2017-03-17 21:34:19 +0800

    python

---
+++ test.py
@@ -0,0 +1,23 @@
+sum =0
+for x in range(101):
+	sum=sum+x
+print(sum)
+
+n=99
+while n>0:
+	sum=sum+n
+	n=n-2
+print(sum)
+
+m=10
+while m>0:
+	m=m-1
+	if m%2==0:
+		print(m)
+		continue
+		
+print(m)
+	
+print('end')
+aaaa
+saaas
